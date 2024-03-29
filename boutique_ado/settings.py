624869c6ab790be7baf88863e05fa3194e7cf57b
Lanre Andero <lanreandero@gmail.com>
2024-03-29 16:44:40 +0000

    added env.py and exported stripe keys

--- boutique_ado/settings.py
+++ boutique_ado/settings.py
@@ -12,6 +12,9 @@ https://docs.djangoproject.com/en/3.2/ref/settings/
 
 import os
 import dj_database_url
+if os.path.exists("env.py"):
+    import env
+
 
 # Build paths inside the project like this: BASE_DIR / "subdir"
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
